1ce4aea3238cbdc3fd902ec71688f4f9749a98d5
Jeppe Klitgaard <hi@jeppe.science>
2023-04-13 16:16:01 +0000

    fix: trial spin

--- ising/primitives2.py
+++ ising/primitives2.py
@@ -36,22 +36,37 @@ def get_random_point_idx(rng_key: RNGKey, shape: TShape) -> TIndex:
     return tuple(idx)
 
 
-def get_trial_spin(*, rng_key:RNGKey, state: State, current_spin: float) -> TSpin:
+def get_trial_spin(*, rng_key: RNGKey, state: State, current_spin: float) -> TSpin:
     with compile_time():
         spin_states = state.env.spin_states
+        print(spin_states)
         spin_states_arr = np.asarray(spin_states)
-        current_spin_idx = jnp.argmax(spin_states_arr == current_spin)
+
+        current_spin_idx = jnp.where(
+            spin_states_arr == current_spin, size=1, fill_value=len(spin_states)
+        )[0][0]
 
         # In order to construct branch of different candidates as a function
         # of current spin we need to use lax.switch statement
         # We an array holding all other possible spins than the current spin
         # We make such an array for each possible current spin
-        branches = [
-            lambda: np.delete(spin_states, i) for i in range(len(spin_states))
-        ]
-        candidates = lax.switch(current_spin_idx, branches)
+        # We need to include random.choice in branches because size of
+        # candidates varies (we could have current_spin not in spins,
+        # in which case we return all possible spins)
+        def branch_maker(i):
+            def branch(rng_key: RNGKey):
+                if i == len(spin_states):
+                    candidates =  spin_states_arr
+                else:
+                    candidates = np.delete(spin_states_arr, i)
 
-    return random.choice(key=rng_key, a=candidates)
+                return random.choice(key=rng_key, a=candidates)
+
+            return branch
+
+        branches = [branch_maker(i) for i in range(len(spin_states) + 1)]
+
+    return lax.switch(current_spin_idx, branches, rng_key)
 
 
 def temperature_to_beta(temperature_or_temperatures: ScalarFloat) -> ScalarFloat:
