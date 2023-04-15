cafd7da4050c7eeeda080ce1608730f5020fc3e9
Jeppe Klitgaard <hi@jeppe.science>
2023-04-15 21:04:46 +0000

    feat: autosave notebooks more often

--- bin/run_experiment.sh
+++ bin/run_experiment.sh
@@ -7,6 +7,8 @@ papermill workbench.ipynb \
     -f "parameters/base.yaml" \
     -f "parameters/$1.yaml" \
     --log-output \
+    --request-save-on-cell-execute \
+    --autosave-cell-every 120 \
     --stdout-file experiments/$1/papermill.log \
     --stderr-file experiments/$1/papermill.log \
     experiments/$1/experiment.ipynb
