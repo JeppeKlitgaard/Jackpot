89519b19ed78cf3b67450ca042f28370604ac2a3
Jeppe Klitgaard <hi@jeppe.science>
2023-04-18 18:58:07 +0000

    feat: update workbench runners

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,17 @@ function run {
     $SCRIPT_FULL_PATH/run_experiment.sh $1
 }
 
-run test
+run "d2_spin0.5_N2"
+run "d2_spin0.5_N4"
+run "d2_spin0.5_N8"
+run "d2_spin0.5_N16"
+run "d2_spin0.5_N32"
+run "d2_spin0.5_N64"
+run "d2_spin0.5_N96"
+run "d2_spin0.5_N128"
+run "d2_spin0.5_N192"
+run "d2_spin0.5_N256"
+run "d2_spin0.5_N320"
+run "d2_spin0.5_N384"
+run "d2_spin0.5_N448"
+run "d2_spin0.5_N512"
