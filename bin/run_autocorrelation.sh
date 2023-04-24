757111e6ca6acf661ab2521d9d1bff8ace93fb0e
Jeppe Klitgaard <hi@jeppe.science>
2023-04-24 17:23:27 +0000

    feat: add autocorrelation runner

---
+++ bin/run_autocorrelation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_FULL_PATH=$(dirname "$0")
+
+function run {
+    $SCRIPT_FULL_PATH/run_experiment.sh $1
+}
+
+run "autocorrelation/N16_wolff"
+run "autocorrelation/N16_metropolis_hastings"
+run "autocorrelation/N32_wolff"
+run "autocorrelation/N32_metropolis_hastings"
+run "autocorrelation/N64_wolff"
+run "autocorrelation/N64_metropolis_hastings"
+run "autocorrelation/N128_wolff"
+run "autocorrelation/N128_metropolis_hastings"
+run "autocorrelation/N256_wolff"
+run "autocorrelation/N256_metropolis_hastings"
+run "autocorrelation/N512_wolff"
+run "autocorrelation/N512_metropolis_hastings"
